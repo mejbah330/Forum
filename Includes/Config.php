57bcc988330f94a1cc357763049b81ca4c5b66ec
mejbah330 <mejbah330@gmail.com>
2025-07-01 22:29:35 -0300

    Create Config.php

---
+++ Includes/Config.php
@@ -0,0 +1,10 @@
+<?php
+// DB configuration constants
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'dynamic_forum');
+define('DB_USER', 'root');
+define('DB_PASS', '');
+
+// Base site URL (no trailing slash)
+define('SITE_URL', 'http://localhost/DynamicForum');
+?>
